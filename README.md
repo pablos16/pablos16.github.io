02e09b920b062279975bf44ca3ef3172e64ed5d3
Javier Rubio <jrubio05@ucm.es>
2020-11-24 11:22:28 +0100

    retoques de archivos y resolución

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# Juego simple con Phaser
+# Regime Police
 
-https://pablos16.github.io/
-https://www.pivotaltracker.com/n/projects/2470816
+Repositorio -> https://pablos16.github.io/
+Organización y seguimiento -> https://www.pivotaltracker.com/n/projects/2470816
 
 ![ra7105xl](https://user-images.githubusercontent.com/62879607/100020834-ca023a80-2de0-11eb-9eda-61216d445e3f.jpg)
--- boot.js
+++ boot.js
@@ -6,13 +6,13 @@ export default class Boot extends Phaser.Scene {
   //Este .js solo sirve para eso y dar comienzo a la escena
   preload() {
     //Personaje
-    this.load.image('player', 'police.png');
+    this.load.image('player', 'resources/game/textures/policeman.png');
 
     //Fondo MAPA
-    this.load.image('map', 'fondoPrueba.png');
+    this.load.image('map', 'resources/game/textures/background.png');
 
     //MUROS
-    this.load.image('Wall', 'muro.png');
+    this.load.image('Wall', 'resources/game/textures/wall.png');
   }
   create() {
     this.scene.start('scene');
