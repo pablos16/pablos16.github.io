dff4794b1360afe49744a5d7981a46b71ee48fb3
Rioni <nrosa01@ucm.es>
2020-12-12 18:56:49 +0100

    Se ha avanzado el sistema de dialogo

--- constants.js
+++ constants.js
@@ -3,7 +3,15 @@ const CT =
      NUM_SLOTS : 8,
      DROPPED_ITEM_SIZE : 0.50,
      gameWidth : 1280,
-     gameHeight : 720
+     gameHeight : 720,
+     cameraZoom : 1,
+     xDialogTextPos: -150,
+     yDialogTextPos: 500,
+     xDialogNamePos: -200,
+     yDialogNamePos: 425,
+     dialogFont : 'Font',
+     dialogSize : 30,
+     dialogAlign: 0
 }
 
 export default CT;
--- scene.js
+++ scene.js
@@ -8,12 +8,12 @@ import CT from './constants.js';
 import NPCDialog from './npcDialog.js';
 import testDialogue from './resources/game/dialogs/testDialog.js'
 
-export default class Scene extends Phaser.Scene{
-  constructor(){
+export default class Scene extends Phaser.Scene {
+  constructor() {
     super({ key: 'scene' });
   }
   //Aqui te crea todo lo que necesites al inicio para todo el juego
-  create(){
+  create() {
     //Deshabilitar menú contextual
     this.input.mouse.disableContextMenu();
 
@@ -23,34 +23,43 @@ export default class Scene extends Phaser.Scene{
 
     //Personaje
     this.player = new Player(this, 200, 300);
-    
-    //NPC
-    this.NPC = new NPCDialog(this,600,300, testDialogue);
-    
-    this.physics.add.overlap(this.player, this.NPC.trigger, (o1, o2) =>{
-      // Si pulsas la E...
-      if (this.player.action.isDown && !this.NPC.isTalking){
-        if(!this.player.isTalking){
-        
-        //Hablas con el
-        //PONER AQUI DIÁLOGO
-        this.player.isTalking = true;
-        this.NPC.isTalking = true;
-        this.NPC.stopX();
-        this.NPC.stopY();
-        this.player.stopX();
-        this.player.stopY();
 
-        //this.testDialogue = new Dialogue(this, 1280/2, 720 - 720/5, 'A: ', 'Hola');
-        this.dialogueImage.setVisible(true);
-        this.NPC.StartDialog()
+    //NPC
+    this.NPC = new NPCDialog(this, 200, 300, testDialogue);
+    //Fondo del dialogo
+    this.dialogueImage = this.add.image(CT.gameWidth / 2, CT.gameHeight / 1.25, 'dialogTest');
+    this.dialogueImage.setScrollFactor(0);
+    this.dialogueImage.setVisible(false);
+
+    this.physics.add.overlap(this.player, this.NPC.trigger, (o1, o2) => {
+      // Si pulsas la E...
+      if (this.player.action.isDown) {
+        if (!this.player.isTalking) {
+
+          //Hablas con el
+          //PONER AQUI DIÁLOGO
+          this.player.isTalking = true;
+          this.NPC.isTalking = true;
+          this.NPC.stopX();
+          this.NPC.stopY();
+          this.player.stopX();
+          this.player.stopY();
+
+          //this.testDialogue = new Dialogue(this, 1280/2, 720 - 720/5, 'A: ', 'Hola');
+          this.dialogueImage.setVisible(true);
+          this.NPC.StartDialog()
         }
-        else{
+        else if(this.NPC.isTalking)
+        {
+          //console.log("Hola");
+          this.NPC.ContinueDialog()
+        }
+        else {
           //Volvemos a mover al personaje
           this.NPC.moveRight();
 
           this.dialogueImage.setVisible(false);
-          this.player.isTalking=false;
+          this.player.isTalking = false;
           this.NPC.isTalking = false;
           //texto.destroy();
         }
@@ -59,21 +68,19 @@ export default class Scene extends Phaser.Scene{
     //this.physics.add.collider(this.player, this.NPCs);
     /*this.physics.add.collider(this.NPCs, this.walls);*/ //Esto debería de sobrar
 
-    //Fondo del dialogo
-    this.dialogueImage = this.add.image(CT.gameWidth/2, CT.gameHeight/1.25, 'dialogTest');
-    this.dialogueImage.setScrollFactor(0);
-    this.dialogueImage.setVisible(false);
-    
-    
+
+
+
     //Barra de alineamiento
     this.align = new Alignment(this, 700, 50);
-    
+
 
     //Cámara que sigue al jugador
     this.cameras.main.startFollow(this.player);
     //TODO CREAR CLASE CON VARIABLES
-    this.cameras.main.width = CT.gameWidth; 
+    this.cameras.main.width = CT.gameWidth;
     this.cameras.main.height = CT.gameHeight;
+    this.cameras.main.zoom = CT.cameraZoom;
 
     //Muros que tendremos que eliminar una vez creemos el tilemap
     this.walls = this.physics.add.staticGroup();
@@ -86,9 +93,9 @@ export default class Scene extends Phaser.Scene{
 
     //Objetos en el suelo
     this.droppedItems = this.physics.add.staticGroup();
-    this.physics.add.overlap(this.player, this.droppedItems, (o1, o2) =>{
+    this.physics.add.overlap(this.player, this.droppedItems, (o1, o2) => {
       // recoger
-      if (this.player.action.isDown){
+      if (this.player.action.isDown) {
         if (this.player.inventory.addItem(o2.id)) o2.destroy();
       }
     });
