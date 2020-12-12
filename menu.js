aa3f6e13bc0e7726622601ab9cfd661f4061d5b8
samuebla <samuebla@ucm.es>
2020-12-12 16:41:31 +0100

    Boton play Implementado

--- menu.js
+++ menu.js
@@ -1,8 +1,6 @@
-import CT from './constants.js';
-
 export default class Menu extends Phaser.Scene{
   constructor(){
-    super({ key: 'scene' });
+    super({ key: 'menu' });
     this.playButton;
     this.controlsButton;
   }
@@ -10,12 +8,18 @@ export default class Menu extends Phaser.Scene{
   create(){
     //Mapa
     this.add.image(640, 400, 'mainMenu');
-    this.playButton = this.add.image(200,400,'play');
+    this.playButton = this.add.image(200,400,'play').setInteractive();
     this.playButton.setScale(5);
-    this.controlsButton = this.add.image(200,525,'controls');
+    this.controlsButton = this.add.image(200,525,'controls').setInteractive();
     this.controlsButton.setScale(5);
 
+    //Si pulsas el botton play
+    this.playButton.on('pointerdown', function(){
+      this.scene.start('scene');
+  },this);
 
+
+  
   }
   
   update(){
