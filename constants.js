688bc391224afd59d8d32fb57160ce56e2fa1484
Javier Rubio <jrubio05@ucm.es>
2020-12-13 21:07:41 +0100

    Update constants.js

--- constants.js
+++ constants.js
@@ -2,10 +2,14 @@ const CT =
 {
      gameWidth: 1280,
      gameHeight: 720,
+     
      cameraZoom: 1,
+
      numSlots: 8,
      inventoryVoid: 0,
+
      droppedItemSize: 0.50,
+
      invBarPosX: 20,
      invBarPosY: 340,
      textPosX: 10,
@@ -14,6 +18,7 @@ const CT =
      textSize: 20,
      textAlign: 0,
      textSpacing: 1,
+
      xDialogTextPos: -175,
      yDialogTextPos: 475,
      xDialogNamePos: -200,
