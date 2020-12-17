fb45fe5f94ea4a1377749d70bdae947866f840d3
Rioni <nrosa01@ucm.es>
2020-12-17 01:04:41 +0100

    Sistema de mision mejorado (no se guardo bien antes)

--- resources/game/misions/misionsDay0.js
+++ resources/game/misions/misionsDay0.js
@@ -2,13 +2,11 @@ const Misions =
     [
         {
             text: "Habla con todos en el pueblo",
-            parts: [],
             completed: 0,
             total: 5
         },
         {
             text: "Habla con el NPC de pruebas",
-            parts: [],
             completed: 0,
             total: 1
         }
