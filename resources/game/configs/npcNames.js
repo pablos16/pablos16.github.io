e27eb6816c358ae9bc33b2f376fc2d59dc7b39a2
Rioni <nrosa01@ucm.es>
2020-12-17 15:42:30 +0100

    Creado archivo con nombres de NPC

---
+++ resources/game/configs/npcNames.js
@@ -0,0 +1,25 @@
+const Names =
+{
+    Police: "Player",
+    Dictator: "Relith",
+    Coronel: "Coronel",
+    BestFriend: "Leandro",
+    Tabernera: "Tabernera",
+    Tabernero: "Tabernero",
+    Paca: "Paca Garte",
+    Sona: "Sona Garte",
+    Empeñista: "Empeñista",
+    Carcelero: "Carcelero Mento",
+    Lola: "Lola Mento",
+    SrtaMento: "Srta Mento",
+    OldMan: "Anciano Solitario",
+    Embajador: "Embajador Lopez",
+    Embajadora: "Señora Lopez",
+    SrtaEmbajadora: "Srta Lopez",
+    MadMan: "Loco",
+    Feriante: "(El) Feriante",
+    Ferianta: "(La) Feriante",
+    SrtaFeriante: "Hija Feriante"
+}
+
+export default Names;
