15bebcd90a02f6f2c8547adabebcc5c0bf22aedf
samuebla <samuebla@ucm.es>
2020-12-18 22:24:34 +0100

    Introducido dialogo Leandro Gado Dia 0

--- resources/game/dialogs/packedDialogs/dialogs0.js
+++ resources/game/dialogs/packedDialogs/dialogs0.js
@@ -1,11 +1,13 @@
 import tabernera from '../day0/tabernera.js'
 import tabernero from '../day0/tabernero.js'
+import leandro from '../day0/leandroGado.js'
 
 
 const dialogs =
 {
     tabernera,
-    tabernero
+    tabernero,
+    leandro
 }
 
 export default dialogs
