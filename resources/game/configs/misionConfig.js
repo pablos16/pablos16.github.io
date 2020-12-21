ecf46635e6ed427f94f7d8d1c77ab00e4f8ef71d
Rioni <nrosa01@ucm.es>
2020-12-21 01:25:03 +0100

    Solucionado bug trigger imagenes

--- resources/game/configs/misionConfig.js
+++ resources/game/configs/misionConfig.js
@@ -2,15 +2,15 @@ import Game from './constants.js'
 
 const CT =
 {
-     misionListX: Game.gameWidth / 2,
-     misionListY: Game.gameHeight,
+     misionListX: Game.gameWidth * 0.8,
+     misionListY: Game.gameHeight * 1.2,
      misionOffsetToggle: 300,
      hideOffset: 50,
 
      misionTextSize: 27.5,
      subMisionTextSize: 20,
-     xMisionText: Game.gameWidth / 8,
-     yMisionText: 0,
+     xMisionText: -212,
+     yMisionText: -144,
      yMissionOffset: 60,
      ySubMisionTextOffset: 25
 }
