d1a1a522e98373cc5370e833118f819826e2104b
Rioni <nrosa01@ucm.es>
2020-12-22 15:26:38 +0100

    Ya no hay bug en el texto

---
+++ resources/game/scripts/libraries/AnimatedText.js
@@ -0,0 +1,35 @@
+import Dialog from '../../configs/dialogConfig.js'
+
+export default class AnimatedText extends Phaser.GameObjects.BitmapText {
+    constructor(scene, text) {
+        super(scene, text.x, text.y, Dialog.dialogFont, text.text
+            , Dialog.dialogSize, Dialog.dialogAlign)
+
+        scene.add.existing(this)
+        this.depth = 100
+        this.setScrollFactor(0)
+
+        let copy = this.text
+        this.text = ""
+        this.i = 0
+        this.timer = scene.time.addEvent({
+            callback: () => {
+                this.text += copy[this.i]
+                this.i++
+            },
+            repeat: copy.length - 1,
+            delay: Dialog.dialogSpeed,
+        })
+
+        this.visible = true
+    }
+
+    stopAnimation() {
+        this.timer.remove(false);
+        this.destroy(true)
+    }
+
+    preUpdate() {
+        console.log("Texto")
+    }
+}
