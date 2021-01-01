5ba5da9b99726e69dffc27280b599b9c1503f03d
samuebla <samuebla@ucm.es>
2021-01-01 18:34:58 +0100

    Escena derrota

--- resources/game/scripts/missionSystem/alignment.js
+++ resources/game/scripts/missionSystem/alignment.js
@@ -1,20 +1,20 @@
 import CT from '../../configs/constants.js';
-export default class Alignment extends Phaser.GameObjects.Container{
-    constructor(scene, x, y, points){
+export default class Alignment extends Phaser.GameObjects.Container {
+    constructor(scene, x, y, points) {
         super(scene, x, y);
 
         this.texture = scene.add.image(x, y, 'bar');//.setScrollFactor(0);
 
-        
+
         scene.add.existing(this);
         this.setScrollFactor(0);
 
         this.points = points;
 
-        this.indicatorTexture = scene.add.image(x,y,'indicator');
+        this.indicatorTexture = scene.add.image(x, y, 'indicator');
         this.add(this.texture);
         this.add(this.indicatorTexture);
-        
+
 
         this.texture.setInteractive();
         this.texture.on('pointerdown', pointer => { this.test() })
@@ -22,23 +22,26 @@ export default class Alignment extends Phaser.GameObjects.Container{
         this.sceneRef = scene
     }
 
-    test(){
+    test() {
         console.log('testing');
         this.addReputation(5);
     }
 
-    addReputation(amount){
+    addReputation(amount) {
         this.points += amount
-        //if(Math.abs(this.points) >= CT.alignmentMaxPoints) //mueres
+        if (Math.abs(this.points) >= CT.alignmentMaxPoints) {
+            //Cargas la escena
+            this.scene.start('lose',{points:100});
+        }
         this.updatePosition(amount)
     }
 
-    updatePosition(amount){
-        
+    updatePosition(amount) {
+
         this.sceneRef.tweens.add({
             targets: this.indicatorTexture,
             duration: 250,
-            y: this.indicatorTexture.y + (CT.alignmentMaxOffset / (CT.alignmentMaxPoints/amount)),
+            y: this.indicatorTexture.y + (CT.alignmentMaxOffset / (CT.alignmentMaxPoints / amount)),
             ease: 'Circ',
         })
     }
