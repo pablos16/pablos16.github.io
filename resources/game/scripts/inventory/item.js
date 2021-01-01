52fd60368088ba1558ea570fdf10f44d67de2044
Javier Rubio <jrubio05@ucm.es>
2021-01-01 19:49:38 +0100

    Update item.js

--- resources/game/scripts/inventory/item.js
+++ resources/game/scripts/inventory/item.js
@@ -3,19 +3,16 @@ import CT from '../../configs/constants.js';
 export default class DroppedItem extends Phaser.GameObjects.Sprite{
     constructor(scene, x, y, id){
         super(scene, x, y, 'debug');
+        this.sc = scene;
         this.changeTo(id);
         this.scene.add.existing(this);
-
-        this.sc = scene;
     }
 
     changeTo(id){
         this.setTexture('items', id);
         this.id = id;
-        this.setOrigin(0);
         this.setScale(CT.droppedItemSize);
-        this.setSize(this.width * CT.droppedItemSize, this.height * CT.droppedItemSize);
-        this.setPosition(this.x - this.width / 2, this.y - this.height / 2);
+        this.y += 8;
     }
 
     checkPickUp(){
