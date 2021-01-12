80c9aa42f41c76a4ce9d0959e7487c79b4d08446
Rioni <nrosa01@ucm.es>
2021-01-12 14:39:50 +0100

    Clase mathFunc comentada

--- resources/game/scripts/libraries/mathFunc.js
+++ resources/game/scripts/libraries/mathFunc.js
@@ -1,3 +1,7 @@
+/**
+ * Normalizes a given vector
+ * @param {vector2} obj vecto2 to normalize (x,y) 
+ */
 export function normalizeVector(obj){
   if (!(obj.x === 0 && obj.y === 0)){
     let module = Math.sqrt(obj.x * obj.x + obj.y * obj.y);
@@ -7,10 +11,19 @@ export function normalizeVector(obj){
   }
 }
 
+/**
+ * Bidimensional module function
+ * @param {number} id number to loop
+ * @param {number} lenght max num id can reach
+ */
 export function loop(id, lenght){
   return (Math.abs(id + lenght) % lenght);
 }
 
+/**
+ * Returns an integer number between 0 and max
+ * @param {number} max maximum number to obtain 
+ */
 export function getRandomInt(max){
   return Math.floor(Math.random() * Math.floor(max));
 }
