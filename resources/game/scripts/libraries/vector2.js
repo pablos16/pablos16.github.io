9fc9a9e3bbd4ac10963a333bf5c80a5769bb058c
Rioni <nrosa01@ucm.es>
2021-01-12 14:40:58 +0100

    Clase vector2 comentada

--- resources/game/scripts/libraries/vector2.js
+++ resources/game/scripts/libraries/vector2.js
@@ -1,3 +1,8 @@
+/**
+ * Simple vector 2 class consisting in two integers (x,y)
+ * @param {number} x x value of the vector
+ * @param {number} y y value of the vector
+ */
 export default class vector2{
     constructor(x,y){
         this.x = x
