feb2be6a3af5396616b08e9e4818b2ba7c81b049
Rioni <nrosa01@ucm.es>
2021-01-13 15:58:10 +0100

    Avances PathList

--- resources/game/scripts/libraries/pathList.js
+++ resources/game/scripts/libraries/pathList.js
@@ -10,39 +10,59 @@ export default class PathInsertor {
         this.context = data.context;
         this.pathName = data.pathName;
 
+        this.test = {
+            path: [
+                {
+                    x: 0,
+                    y: 0,
+                    delay: 150,
+                },
+                {
+                    x: 100,
+                    y: 0,
+                    delay: 150,
+                },
+                {
+                    x: -50,
+                    y: 100,
+                    delay: 150,
+                },
+            ],
+            onFinish: () => { },
+        }
+
         this.inserPath()
     }
 
     inserPath() {
-        let path = this[this.pathName];
-        path(this);
+        this.move(this.test)
     }
 
-    default(ctx)
-    {
-       ctx.context.path = new PathFollower({
-            path: [
-                new PathNode({
-                    x: ctx.body.position.x,
-                    y: ctx.body.position.y,
-                    delay: 3500,
-                }),
-                new PathNode({
-                    x: ctx.body.position.x + 100,
-                    y: ctx.body.position.y,
-                    delay: 1500,
-                }),
-                new PathNode({
-                    x: ctx.body.position.x + 50,
-                    y: ctx.body.position.y + 100,
-                    delay: 400,
-                })
-            ],
-            sceneRef: ctx.scene,
-            body: ctx.body,
+    move(data) {
+        this.context.path = new PathFollower({
+            path: this.generatePathFromObject(data.path),
+            sceneRef: this.scene,
+            body: this.body,
             loop: true,
-            onFinish: (pathFollower) => {  }
+            onFinish: (pathFollower) => { data.onFinish() }
         })
+    }
+
+    generatePathFromObject(path) {
+        let paths = [];
+        let pathLengh = path.length;
+        let xTotal = 0;
+        let yTotal = 0;
+        for (let i = 0; i < pathLengh; i++) {
+            xTotal += path[i].x;
+            yTotal += path[i].y;
+            paths.push(new PathNode({
+                x: this.body.position.x + xTotal,
+                y: this.body.position.y + yTotal,
+                delay: path[i].delay,
+            }))
+        }
+        return paths;
     }
 }
 
