7ccddb265e4c4111da79faf35ed1a20b56023124
Rioni <nrosa01@ucm.es>
2021-01-13 13:53:48 +0100

    Avances implementacion path Insertor

--- resources/game/scripts/libraries/pathList.js
+++ resources/game/scripts/libraries/pathList.js
@@ -10,35 +10,36 @@ export default class PathInsertor {
         this.context = data.context;
         this.pathName = data.pathName;
 
-        this.inserPath(this[this.pathName])
+        this.inserPath()
     }
 
-    inserPath(path) {
-     this.default();
+    inserPath() {
+        let path = this[this.pathName];
+        path(this);
     }
 
-    default()
+    default(ctx)
     {
         new PathFollower({
             path: [
                 new PathNode({
-                    x: this.body.position.x,
-                    y: this.body.position.y,
+                    x: ctx.body.position.x,
+                    y: ctx.body.position.y,
                     delay: 3500,
                 }),
                 new PathNode({
-                    x: this.body.position.x + 100,
-                    y: this.body.position.y,
+                    x: ctx.body.position.x + 100,
+                    y: ctx.body.position.y,
                     delay: 1500,
                 }),
                 new PathNode({
-                    x: this.body.position.x + 50,
-                    y: this.body.position.y + 100,
+                    x: ctx.body.position.x + 50,
+                    y: ctx.body.position.y + 100,
                     delay: 400,
                 })
             ],
-            sceneRef: this.scene,
-            body: this.body,
+            sceneRef: ctx.scene,
+            body: ctx.body,
             onFinish: (context) => { context.currentPath = 0 }
         })
     }
