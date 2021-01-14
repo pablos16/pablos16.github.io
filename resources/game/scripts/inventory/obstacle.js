088f8c9c7ef65f59008f3a4d7eb5a531094e97cb
Javier Rubio <jrubio05@ucm.es>
2021-01-14 12:24:06 +0100

    ligeros ajustes en el mapa y mejora de obstacle.js en el uso de ítems alternativos

--- resources/game/scripts/inventory/obstacle.js
+++ resources/game/scripts/inventory/obstacle.js
@@ -1,8 +1,11 @@
 export default class Obstacle extends Phaser.GameObjects.Image{
-    constructor(scene, x, y, texture, requirement, functS, functF){
+    constructor(scene, x, y, texture, requirement, functS, functF, altRequirement, altFunctS){
         super(scene, x, y, texture);
         this.setInteractive();
+
         this.requires = requirement;
+        if (altRequirement !== undefined) this.requiresAlt = altRequirement;
+
         if (functS == undefined){
             this.success = function(){
                 console.log('recibido');
@@ -10,23 +13,35 @@ export default class Obstacle extends Phaser.GameObjects.Image{
             };
         }
         else{
-            console.log('zeta')
             this.success = functS;
         }
+
         if (functF == undefined){
             this.failure = function(){
-                console.log('quiero ' + this.requires);
+                console.log('quiero ' + this.requires + '; alt: ' + this.requiresAlt);
             };
         }
         else{
-            console.log('zeta2')
             this.failure = functF;
         }
+
+        if (altFunctS == undefined){
+            this.successAlt = this.success;
+        }
+        else{
+            this.successAlt = altFunctS;
+        }
+
         scene.add.existing(this);
+
         this.on('pointerdown', pointer =>{
             if (scene.player.inventory.getItemAt(scene.inventoryBar.selection) === this.requires){
                 scene.inventoryBar.useCurrentItem();
                 this.success();
+            }
+            else if (this.requiresAlt !== undefined && scene.player.inventory.getItemAt(scene.inventoryBar.selection) === this.requiresAlt){
+                scene.inventoryBar.useCurrentItem();
+                this.successAlt();
             }
             else{
                 this.failure();
