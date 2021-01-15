f4ba4b44fa91897540d9828cdb4caf69d5806500
samuebla <samuebla@ucm.es>
2021-01-15 18:30:02 +0100

    POR FIN SOLUCIONADO

---
+++ resources/game/dialogs/day2/ferianteHijo.js
@@ -0,0 +1,28 @@
+import Names from '../../configs/npcNames.js'
+
+//TODO ACABAR PERSONALIDAD FERIANTA. PREGUNTAR A SAMUEL
+const Dialog =
+    [
+        {
+            id: 0,
+            name: Names.SrFeriante,
+            text: ["Blin Blin","Pe-pe-pe...","Weeeee"],
+            state: [
+                {
+                    targetState: ["any"],
+                    nextIndex: -1
+                },
+            ]
+        },
+        {
+            id: -1,
+            state: [
+                {
+                    targetState: [0],
+                    nextIndex: 0
+                }
+            ]
+        }
+    ]
+
+export default Dialog;
