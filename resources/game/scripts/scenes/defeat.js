1425d3adc91d7fcb3fc644baa4ea63d36a7cd849
Rioni <nrosa01@ucm.es>
2021-01-20 14:12:27 +0100

    Se han añadido los sonidos a la escena de victoria y derrota

--- resources/game/scripts/scenes/defeat.js
+++ resources/game/scripts/scenes/defeat.js
@@ -9,6 +9,8 @@ export default class Defeat extends Phaser.Scene {
   init(data) {
     // Guardamos los puntos
     this.points = data.points;
+    this.musicVolume = data.musicVolume
+    this.soundVolume = data.soundVolume
   }
 
   create() {
@@ -20,7 +22,15 @@ export default class Defeat extends Phaser.Scene {
 
     // Música
     this.music = this.sound.add('defeat', CT.backgroundMusic);
+    this.musicList = [this.music]
+    this.soundList = []
+    this.soundList.push(this.slider = this.sound.add('slider', CT.effectSounds))
+    this.soundList.push(this.sliderEnd = this.sound.add('sliderEnd', CT.effectSounds))
+    if (this.musicVolume) this.musicList[0].volume = this.musicVolume;
+    if (this.soundVolume) this.soundList[0].volume = this.soundVolume;
+    
     this.music.play();
+
 
     // Pantalla
     let img = (this.points >= 0) ? 'loseRegime' : 'loseVillage';
